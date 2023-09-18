ab3f9999f62c15549d9446abe0de25086a1cb7ad
Jill Mudgette <peoplewithpassions@gmail.com>
2023-09-17 18:01:21 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/togetherraveAPI/api.js
+++ store/togetherraveAPI/api.js
@@ -1,58 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const togetherraveAPI = axios.create({
   baseURL: "https://togetherrave-43428.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return togetherraveAPI.post(`/api/v1/login/`, payload.data)
+  return togetherraveAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return togetherraveAPI.post(`/api/v1/signup/`, payload.data)
+  return togetherraveAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return togetherraveAPI.get(`/rest-auth/user/`)
+  return togetherraveAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return togetherraveAPI.put(`/rest-auth/user/`, payload.data)
+  return togetherraveAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return togetherraveAPI.patch(`/rest-auth/user/`, payload.data)
+  return togetherraveAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return togetherraveAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return togetherraveAPI.post(`/rest-auth/login/`, payload.data)
+  return togetherraveAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return togetherraveAPI.get(`/rest-auth/logout/`)
+  return togetherraveAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return togetherraveAPI.post(`/rest-auth/logout/`)
+  return togetherraveAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return togetherraveAPI.post(`/rest-auth/registration/`, payload.data)
+  return togetherraveAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return togetherraveAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return togetherraveAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return togetherraveAPI.post(`/rest-auth/password/change/`, payload.data)
+  return togetherraveAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return togetherraveAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return togetherraveAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return togetherraveAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return togetherraveAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -68,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
