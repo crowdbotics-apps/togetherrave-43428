df0d40df7b5cf43073d7d96996fcd78b1395ed63
Jill Mudgette <peoplewithpassions@gmail.com>
2023-09-18 02:25:35 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled11/index.js
+++ screens/Untitled11/index.js
@@ -1,15 +1,46 @@
-import React from "react";
-import { StyleSheet, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, View, TextInput, Button, FlatList, Text, StyleSheet } from "react-native";
 
-const Untitled11 = () => {
-  return <SafeAreaView style={styles.safeArea}>
-        
+const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  const sendMessage = () => {
+    setMessages([...messages, {
+      id: messages.length.toString(),
+      text: input
+    }]);
+    setInput("");
+  };
+
+  return <SafeAreaView style={styles.container}>
+      <FlatList data={messages} keyExtractor={item => item.id} renderItem={({
+      item
+    }) => <Text>{item.text}</Text>} />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} value={input} onChangeText={text => setInput(text)} />
+        <Button title="Send" onPress={sendMessage} />
+      </View>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
-  safeArea: {
-    height: '100%'
+  container: {
+    flex: 1
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    marginBottom: 10
+  },
+  input: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    padding: 5
   }
 });
-export default Untitled11;
+export default ChatScreen;
