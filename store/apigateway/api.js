ce54ad5d64d4417c654f617c7301e35e5b21ca5a
Jill Mudgette <peoplewithpassions@gmail.com>
2023-09-18 03:15:51 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/apigateway/api.js
+++ store/apigateway/api.js
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const apigateway = axios.create({
   baseURL: "https://yotqlxh81k.execute-api.us-west-2.amazonaws.com/prod",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function apigateway_post__create(payload) {
-  return apigateway.post(`/`, payload.data)
+  return apigateway.post(`/`, payload.data);
 }
-export const apiService = { apigateway_post__create }
+
+export const apiService = {
+  apigateway_post__create
+};
--- store/eventBright/eventbright_response_get_ListCategories.slice.js
+++ store/eventBright/eventbright_response_get_ListCategories.slice.js
@@ -1,13 +1,16 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const eventbright_get_categories_list = createAsyncThunk(
-  "eventbright_response_get_ListCategories/eventbright_get_categories_list",
-  async payload => {
-    const response = await apiService.eventbright_get_categories_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const eventbright_get_categories_list = createAsyncThunk("eventbright_response_get_ListCategories/eventbright_get_categories_list", async payload => {
+  const response = await apiService.eventbright_get_categories_list(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const eventbright_response_get_ListCategoriesSlice = createSlice({
   name: "eventbright_response_get_ListCategories",
   initialState,
@@ -15,24 +18,24 @@ const eventbright_response_get_ListCategoriesSlice = createSlice({
   extraReducers: {
     [eventbright_get_categories_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [eventbright_get_categories_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
+        state.entities = action.payload;
+        state.api.loading = "idle";
       }
     },
     [eventbright_get_categories_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   eventbright_get_categories_list,
   slice: eventbright_response_get_ListCategoriesSlice
-}
+};
