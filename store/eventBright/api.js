6e07a20bcc861d152608766cfc756223ece387e9
Jill Mudgette <peoplewithpassions@gmail.com>
2023-09-17 22:51:05 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/eventBright/api.js
+++ store/eventBright/api.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-import { EVENT_BRIGHT_INTEGRATION_TOKEN } from "react-native-dotenv"
+import axios from "axios";
+import { EVENT_BRIGHT_INTEGRATION_TOKEN } from "react-native-dotenv";
 const eventBright = axios.create({
   baseURL: "https://www.eventbriteapi.com/v3",
   headers: {
@@ -7,10 +7,12 @@ const eventBright = axios.create({
     "Content-Type": "application/json",
     Authorization: `Bearer ${EVENT_BRIGHT_INTEGRATION_TOKEN}`
   }
-})
+});
+
 function eventbright_get_categories_list(payload) {
-  return eventBright.get(`/categories/`)
+  return eventBright.get(`/categories/`);
 }
+
 function eventbright_get_events_event_id_attendees_read(payload) {
   return eventBright.get(`/events/${payload.event_id}/attendees/`, {
     params: {
@@ -19,9 +21,10 @@ function eventbright_get_events_event_id_attendees_read(payload) {
       last_item_seen: payload.last_item_seen,
       attendee_ids: payload.attendee_ids
     }
-  })
+  });
 }
+
 export const apiService = {
   eventbright_get_categories_list,
   eventbright_get_events_event_id_attendees_read
-}
+};
